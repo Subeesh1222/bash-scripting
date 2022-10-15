553dd3a34ae30def5c5a1a156a654f6e6c721c9e
Subee <subeeeesh@gmail.com>
2022-10-15 11:33:30 +0530

    few changes

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -3,3 +3,5 @@
 # only one shebang
 # Shebang is the character sequence consisting of the characters number sign and exclamation mark (#!) at the beginning of a script
 echo "Hello to bash scripting world"
+echo hai
+echo Hello
