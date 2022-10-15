a85291df083c7caf2586392ca0b9fc651e5b1c73
Subee <subeeeesh@gmail.com>
2022-10-15 12:17:16 +0530

    escape sequence

--- 03-color.sh
+++ 03-color.sh
@@ -6,3 +6,6 @@
 # echo -e "\e[32m TextOfYourChoice  \e[0m"
 
 echo -e "\e[34m To check the color \e[0m"
+
+echo -e "\e[33m DevOps has a very great future \e[0m"
+echo -e "\e[43;37m DevOps has a very great future \e[0m"
