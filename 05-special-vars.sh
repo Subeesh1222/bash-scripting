c199123467c3a247807de5b9daf093f1987b7356
Subee <subeeeesh@gmail.com>
2022-10-16 09:59:06 +0530

    special variable  to

---
+++ 05-special-vars.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Here are the special variables
+
+ # $0        : This will gives the name of the script you're running 
+ # $1 to $9  :You can pass a maximum of 9 variables from the command line when you're running the script 
+ # $*
+ # $@
+ # $#
+
+echo "script name running is $0"
+
+a=10
+b=$1
+c=$2
+d=$3
+
+echo "value of a is $a"
+echo "value of b is $b"
+echo "value of c is $c"
+echo "value of d is $d"
+## sh scriptname.sh 100 200 300
+## sh scriptname.sh $1 $2 $3
