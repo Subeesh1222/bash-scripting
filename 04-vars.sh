f81b62a19720b16943b110f512c91ca7d5d5e71b
Subee <subeeeesh@gmail.com>
2022-10-16 06:40:16 +0530

    variable declaration

---
+++ 04-vars.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+a =10
+#a is 10 and it is an integer.
+
+b = def
+#b is def and it a string.
+
+###Here, there is no datatype concept in bash scripting. Everything is string by default
+
+#shell scripting is came up with the UNIX. Since they have re-writtening LINUX they created one more terminal called BASH.
+#BASH : Born Again Shell
+
+# We have declared a variable and now print it
+# Various ways to print it
+
+echo $a  # it prints the value of a
+echo ${a}
+echo "{b}"
+echo value of a is : $a
+echo value od b is : $b
+
---
+++ general info.md
@@ -0,0 +1,4 @@
+###Here, there is no datatype concept in bash scripting. Everything is string by default
+
+#shell scripting is came up with the UNIX. Since they have re-writtening LINUX they created one more terminal called BASH.
+#BASH : Born Again Shell
