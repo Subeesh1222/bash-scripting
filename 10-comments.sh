d76713bc5986e4bc08172c33b399a567eca3388e
Subee <subeeeesh@gmail.com>
2022-10-18 12:20:15 +0530

    comments

---
+++ 10-comments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#multiple line comments , this to prevent the stop executing certain code if the we loop in multi line
+
+echo "print here"
+echo "hello world"
+
+<<< COMMENT
+echo "cloud training"
+echo "bash comments"
+
+COMMENT
